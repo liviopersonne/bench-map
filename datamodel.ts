a4a6ed6b1d64c4b1d4b1ba056f3e0fe82daa1258
livio <livio.personne@gmail.com>
2025-11-25 13:15:54 +0100

    detamodel plus détaillé

--- datamodel.ts
+++ datamodel.ts
@@ -1,11 +1,21 @@
-enum Material {}
+enum Material {
+    Wood,
+    Metal,
+}
+
+enum BenchType {
+    Normal,
+    BusStop,
+    Picnic,
+}
 
 type Bench = {
     xcoord: number,
     ycoord: number,
-    backrest?: boolean,
-    picnic?: boolean,
-    covered?: boolean,
+    type?: BenchType,
     material?: Material,
+    backrest?: boolean,
+    covered?: boolean,
+    capacity?: number,
     pov?: ImageData,
 }
