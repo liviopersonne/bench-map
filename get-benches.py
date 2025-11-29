7e241a930c4a666452f52311f71e799859377d45
livio <livio.personne@gmail.com>
2025-11-29 19:57:42 +0100

    ajout du parsing

--- get-benches.py
+++ get-benches.py
@@ -1,16 +1,37 @@
 import overpass
+import json
+import os
+import csv
 
 api = overpass.API()
 
 border = [48.710928, 2.186049, 48.719715, 2.221645]
 bbox = f"{','.join(map(str, border))}"
+query = f"""node["amenity"="bench"]({bbox});"""
 
-query = f"""
-[bbox:{bbox}];
-node[amenity=bench];
-out geom;
-"""
+def get_json(outfile: str):
+    assert(outfile.endswith("json"))
+    data = api.get(query, verbosity="geom", responseformat="json")["elements"]
+    file = open(outfile, "w")
+    json.dump(data, file)
+    file.close()
 
-data = api.get(query, verbosity="geom")
+def get_csv(outfile:str):
+    assert(outfile.endswith("csv"))
+    format = 'csv(::id,::lat,::lon,"amenity","backrest","crossing","highway","tactile_paving","colour","material","seats")'
+    data = api.get(query, verbosity="geom", responseformat=format)
+    file = open(outfile, "w")
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(data)
+    file.close()
 
-print(len(data.features))
+
+if __name__ == '__main__':
+    basedir = "dumps"
+    os.makedirs(basedir, exist_ok=True)
+
+    jsonfile = os.path.join(basedir, "benches.json")
+    get_json(jsonfile)
+
+    csvfile = os.path.join(basedir, "benches.csv")
+    get_csv(csvfile)
